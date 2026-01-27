36f39c2ff14854dfa0eb9a617a95201d09b0b2c0
Nguyễn Đức Anh <78894062+kiencang@users.noreply.github.com>
2026-01-27 13:49:59 +0700

    Thêm thông tin phiên bản vào phần giao diện

--- simple-cafe-purge.php
+++ simple-cafe-purge.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Simple Cafe Purge
  * Description: Giải pháp xóa cache Cloudflare siêu nhẹ cho Blog. Tự động xóa khi cập nhật bài viết và hỗ trợ nút "Purge Everything".
- * Version: 1.13.5
+ * Version: 1.13.6
  * Author: wpsila - Nguyễn Đức Anh
  * Author URI: https://simple-cafe-purge.wpsila.com
  */
@@ -81,7 +81,7 @@ function wpsila_scfp_options_page() {
     </style>
 
     <div class="wrap">
-        <h1>☕ Simple Cafe Purge</h1>
+        <h1>☕ Simple Cafe Purge v1.13.6</h1>
         <p>Plugin siêu nhẹ giúp đồng bộ cache giữa WordPress và hệ thống của Cloudflare.</p>
         <hr>
         
